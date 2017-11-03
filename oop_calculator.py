ee0b5cee28ab4184109f7cd0067f1bbb56516dc5
WilliamTheBeginner <william78smart@hotmail.com>
2017-11-03 14:15:50 -0400

    new

--- oop_calculator.py
+++ oop_calculator.py
@@ -1,4 +1,4 @@
-class Calculator():
+"""class Calculator():
     def add(v1, v2):
         return v1 + v2
     def subtract(v3, v4):
@@ -17,3 +17,30 @@ print(Calculator.multiply(3, 6))
 print(Calculator.divide(10, 5))
 
 # if there is no self, you don't need to provide an instance for it
+"""
+class Employee():
+    count = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.count += 0
+        print('Initialized Employee {}'.format(self.name))
+
+    def raise_sal(self, raiseamt):
+        self.salary += raiseamt
+        #return salary doesn't really raise
+        print(self.salary)
+        print('Raising salary of {} by {}.'.format(self.name, raiseamt))
+        print('New salary is {}'.format(self.salary))
+    def cut_sal(self, cutamt):
+        self.salary -= cutamt
+        print(self.salary)
+        print('Cutting salary of {} by {}.'.format(self.name, cutamt))
+        print('New Salary is {}'.format(self.salary))
+
+e1 = Employee('Josh', 20)
+e1.raise_sal(0.20)
+e2 = Employee('Ravi', 10)
+e2.raise_sal(0.01)
+e1.cut_sal(20)
