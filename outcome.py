520cc271e51f7f09bfd1de55223569045b3de5ac
WilliamTheBeginner <william78smart@hotmail.com>
2017-11-13 21:19:49 -0500

    lol

--- outcome.py
+++ outcome.py
@@ -1,25 +1,25 @@
 class Outcome:
-    set1 = []
+
     def __init__(self, name, odds):
         self.name = name
         self.odds = odds
-        set1.insert(self)
+
     def __eq__(self, other):
-        return self.name == other.name and self.odds == other.odds
+        return self.name == other.name and self.oods == other.odds
+
+    def __ne__(self, other):
+        return self.name != other.name and self.oods != other.odds
+
     def __hash__(self):
         return hash((self.name, self.odds))
-oc1 = Outcome('Red', 1)
-oc2 = Outcome('Red', 1)
-oc3 = Outcome('Black', 1)
-oc4 = Outcome('1', 35)
-oc5 = Outcome('1', 11)
-# hash is the same for both objects
-print(hash(oc1))
-print(hash(oc2))
-# both objects are equal
-print(oc1 == oc2)
 
-if oc1.name == oc2.name:
-    print(True)
-else:
-    print(False)
+    def winAmount(self, amount):
+        self.odds * self.amount
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+    
+oc1 = Outcome('1', )
