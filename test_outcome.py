a56b17affa744a151788d56ca080281e0acd5f61
WilliamTheBorg <william78smart@hotmail.com>
2017-11-14 17:58:33 -0500

    lol

--- test_outcome.py
+++ test_outcome.py
@@ -1,3 +1,16 @@
 # write test_ and what you're testing
 import unittest
 import outcome
+
+
+def add(x, y):
+    return x + y
+
+
+class Testadd(unittest.TestCase):
+    def test(self):
+        self.assertEqual(add(1, 3), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
