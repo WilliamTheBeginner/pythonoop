1a05e5818db4d117f88695c2ef38f896bd3c4213
WilliamTheBeginner <william78smart@hotmail.com>
2017-11-15 09:42:38 -0500

    Hello

--- bin.py
+++ bin.py
@@ -1,7 +1,9 @@
 from outcome import Outcome
-
+# a spin of 1 reflects the 1 bin and the outcomes in the bin
+# all the outcomes of 1 are collected in a single bin
 class Bin(frozenset):
     pass
 
-# a spin of 1 reflects the 1 bin and the outcomes in the bin
-# all the outcomes of 1 are collected in a single bin
+five = Bin([Outcome('00-0-1-2-3', 6)])
+zero = Bin([Outcome('0', 35), five])
+zerozero = Bin([Outcome('00', 35), five])
