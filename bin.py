f70b81899d525858b358aff34f564357382a4f06
WilliamTheBorg <william78smart@hotmail.com>
2017-11-19 12:35:24 -0500

    1

--- bin.py
+++ bin.py
@@ -1,8 +1,11 @@
 from outcome import Outcome
 # a spin of 1 reflects the 1 bin and the outcomes in the bin
 # all the outcomes of 1 are collected in a single bin
+
+
 class Bin(frozenset):
     pass
+
 
 red = Outcome('Red', 1)
 black = Outcome('Black', 1)
@@ -65,16 +68,39 @@ line101112131415 = Outcome('Line 10-11-12-13-14-15', 5)
 
 zero = Bin([Outcome('0', 35), five])
 zerozero = Bin([Outcome('00', 35), five])
-one = Bin([Outcome('1', 35), red, low, odd, dozen_1, column_1, split12, split14, street123, corner1245, five, line123456])
-two = Bin([Outcome('2', 35), black, low, even, dozen_1, column_2, split12, split23, split25, street123, corner1245, corner2356, five, line123456])
-three = Bin([Outcome('3', 35), red, low, odd, dozen_1, column_3, split23, split36, street123, corner2356, five, line123456])
-four = Bin([Outcome('4', 35), black, low, even, dozen_1, column_1, split14, split45, split47, street456, corner1245, corner4578, line123456, line456789])
-five = Bin([Outcome('5', 35), red, low, odd, dozen_1, column_2, split45, split25, split56, split58, street456, corner1245, corner2356, corner4578, corner5689, line123456, line456789])
-six = Bin([Outcome('6', 35), black, low, even, dozen_1, column_3, split36, split56, split69, street456, corner2356, corner5689, line456789])
-seven = Bin([Outcome('7', 35), red, low, odd, dozen_1, column_1, split47, split78, split710, street789, corner781011, corner4578, line456789, line789101112])
-eight = Bin([Outcome('8', 35), black, low, even, dozen_1, column_2, split78, split58, split89, split811, street789, corner5689, corner4578, corner891112, corner781011, line456789, line789101112])
-nine = Bin([Outcome('9', 35), red, low, odd, dozen_1, column_3, split89, split69, split912, street789, corner5689, corner891112, line456789, line789101112])
-ten = Bin([Outcome('10', 35), black, low, even, dozen_1, column_1, split710, split1011, split1013, street101112, corner781011, corner10111314, line789101112, line101112131415])
-eleven = Bin([Outcome('11', 35), black, low, odd, dozen_1, column_2, split811, split1011, split1112, split1114, street101112, corner781011, corner10111314, corner891112, corner11121415, line789101112, line101112131415])
-twelve = Bin([Outcome('12', 35), red, low, even, dozen_1, column_3, split912, split1112, split1215, street101112, corner891112, corner11121415, line789101112, line101112131415])
-thirteen = Bin([Outcome('13' 35), black, low, odd, dozen_2, column_1, split1013, split1314, split1316, street131415, st])
+one = Bin([Outcome('1', 35), red, low, odd, dozen_1, column_1,
+           split12, split14, street123, corner1245, five, line123456])
+two = Bin([Outcome('2', 35), black, low, even, dozen_1, column_2, split12,
+           split23, split25, street123, corner1245, corner2356, five, line123456])
+three = Bin([Outcome('3', 35), red, low, odd, dozen_1, column_3,
+             split23, split36, street123, corner2356, five, line123456])
+four = Bin([Outcome('4', 35), black, low, even, dozen_1, column_1, split14, split45,
+            split47, street456, corner1245, corner4578, line123456, line456789])
+five = Bin([Outcome('5', 35), red, low, odd, dozen_1, column_2, split45, split25, split56,
+            split58, street456, corner1245, corner2356, corner4578, corner5689, line123456, line456789])
+six = Bin([Outcome('6', 35), black, low, even, dozen_1, column_3, split36,
+           split56, split69, street456, corner2356, corner5689, line456789])
+seven = Bin([Outcome('7', 35), red, low, odd, dozen_1, column_1, split47, split78,
+             split710, street789, corner781011, corner4578, line456789, line789101112])
+eight = Bin([Outcome('8', 35), black, low, even, dozen_1, column_2, split78, split58, split89,
+             split811, street789, corner5689, corner4578, corner891112, corner781011, line456789, line789101112])
+nine = Bin([Outcome('9', 35), red, low, odd, dozen_1, column_3, split89, split69,
+            split912, street789, corner5689, corner891112, line456789, line789101112])
+ten = Bin([Outcome('10', 35), black, low, even, dozen_1, column_1, split710, split1011,
+           split1013, street101112, corner781011, corner10111314, line789101112, line101112131415])
+eleven = Bin([Outcome('11', 35), black, low, odd, dozen_1, column_2, split811, split1011, split1112, split1114,
+              street101112, corner781011, corner10111314, corner891112, corner11121415, line789101112, line101112131415])
+twelve = Bin([Outcome('12', 35), red, low, even, dozen_1, column_3, split912, split1112,
+              split1215, street101112, corner891112, corner11121415, line789101112, line101112131415])
+thirteen = Bin([Outcome('13' 35), black, low, odd, dozen_2, column_1, split1013, split1314, split1316, street131415, corner10111314, corner13141617, line1011121314, line131415161718])
+fourteen = Bin([Outcome('14', 35), red, low, even, dozen_2, column_2, split1314, split1114, split1415, split1417,
+                street131415, corner10111314, corner11121415, corner14151718, corner13141617, line101112131415, line131415161718])
+fifteen = Bin([Outcome('15', 35), black, low, odd, dozen_2, column_3, split1215, split1415,
+               split1518, street131415, corner11121415, corner14151718, line101112131415, line131415161718])
+sixteen = Bin([Outcome('16', 35), red, low, even, dozen_2, column_1, split1316, split1617,
+               split1619, street161718, corner13141617, corner16171920, line131415161718, line161718192021])
+seventeen = Bin([Outcome('17', 35), black, low, odd, dozen_2, column_2, split1417, split1617, split1718, split1720,
+                 street161718, corner14151718, corner17182021, corner13141617, corner16171920, line131415161718, line161718192021])
+eighteen = Bin([Outcome('18', 35), red, low, even, dozen_2, column_3, split1718, split1518,
+                split1821, street161718, corner14151718, corner17182021, line131415161718, line161718192021])
+nineteen = Bin([Outcome('19', 35), red, high, even, dozen_2, column_1, split1619, ])
